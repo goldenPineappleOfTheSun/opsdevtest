42cde1a141f2519063fc1658d62d3ee7c7d0df7d
saetov <goldenpineappleofthesun@gmail.com>
2021-12-21 08:38:22 +0500

    oh

---
+++ Controllers/ConvertController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PdfToImage.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PdfToImage.Controllers
+{
+    public class ConvertController : Controller
+    {
+        public int Index()
+        {
+            return 43;
+        }
+    }
+}
